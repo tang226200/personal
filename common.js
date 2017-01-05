e560707436a783c36f4ea61a1f4d07bc5b0db921
tangdaye <55187735@qq.com>
2017-01-05 09:56:24 +0800

    first

---
+++ common.js
@@ -0,0 +1,24 @@
+var {
+	PI,
+	random,
+	atan2,
+	sqrt,
+	pow
+} = Math;
+
+function rnd(n,m){
+	return parseInt(random()*(m-n)+n)
+}
+
+function d2a(d){
+	return d/180*PI;
+}
+
+function a2d(a){
+	return a*180/PI;
+}
+
+function clear(ctx){
+	ctx.clearRect(0,0,canvas.width,canvas.height)
+}
+
